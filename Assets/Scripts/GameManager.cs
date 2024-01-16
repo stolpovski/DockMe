3d2bd7ffa82fc14bb909b27df50d8ca19113e784
stolpovski <stolpovski@gmail.com>
2024-01-16 20:47:10 +0300

    ps fixed

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,6 +8,11 @@ public class GameManager : MonoBehaviourPunCallbacks
 {
     [Tooltip("The prefab to use for representing the player")]
     public GameObject playerPrefab;
+
+    public bool IsRotate;
+
+    private Quaternion _rotation = Quaternion.identity;
+
     public override void OnPlayerEnteredRoom(Player other)
     {
         Debug.LogFormat("OnPlayerEnteredRoom() {0}", other.NickName); // not seen if you're the player connecting
@@ -40,9 +45,9 @@ public class GameManager : MonoBehaviourPunCallbacks
         }
         else
         {
-            Quaternion rotation = Quaternion.Euler(Random.Range(0, 360), Random.Range(0, 360), Random.Range(0, 360));
+            if (IsRotate) _rotation = Quaternion.Euler(Random.Range(0, 360), Random.Range(0, 360), Random.Range(0, 360));
             // we're in a room. spawn a character for the local player. it gets synced by using PhotonNetwork.Instantiate
-            PhotonNetwork.Instantiate(this.playerPrefab.name, new Vector3(Random.Range(-20, 20), Random.Range(-20, 20), Random.Range(-20, 20)), rotation, 0);
+            PhotonNetwork.Instantiate(this.playerPrefab.name, new Vector3(Random.Range(-20, 20), Random.Range(-20, 20), Random.Range(-20, 20)), _rotation, 0);
         }
     }
 }
